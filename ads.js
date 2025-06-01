be38d0f19f621121a2ca8b921a3a5860acb56740
Hà Phong <87749521+doanchifong@users.noreply.github.com>
2025-06-01 17:15:16 +0700

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,7 @@
+(function() {
+  var ad = document.createElement('div');
+  ad.id = 'ad-banner';
+  ad.innerText = 'This is a test ad';
+  ad.style.display = 'none';
+  document.body.appendChild(ad);
+})();
